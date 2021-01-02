76192908452bdd5af19105a8814c95ac936f3ede
shayan <42shayan42@gmail.com>
2021-01-02 01:09:12 -0800

    init commit

--- Codepath-prework/ViewController.swift
+++ Codepath-prework/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func didTapButton(_ sender: Any) {
+        // console logging
+        print("Text color change invoked")
+        
+        // change text color
+        textLabel.textColor = UIColor.orange
+        
+        
+    }
 }
 
