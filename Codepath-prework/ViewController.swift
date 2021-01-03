afb2f685fdbb5382b8b2d39165d81063f27af526
shayan <42shayan42@gmail.com>
2021-01-02 17:33:03 -0800

    fixes

--- Codepath-prework/ViewController.swift
+++ Codepath-prework/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
     
-    
+    @IBOutlet weak var textField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -27,5 +28,28 @@ class ViewController: UIViewController {
         
         
     }
+    
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        
+        view.backgroundColor = UIColor.red
+        
+    }
+    
+    
+    @IBAction func didTapTextButton(_ sender: Any) {
+        
+        // textLabel.text = "peace out!"
+        textLabel.text = textField.text
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+        
+        textLabel.text  = "Hello from Ethan Shayan Sarnevesht!"
+        view.backgroundColor = UIColor(red: (232/255.0), green: (255/255.0), blue: (235/255.0), alpha: 1.0)
+        textLabel.textColor = UIColor(red: (162/255.0), green: (92/255.0), blue: (215/255.0), alpha: 1.0)
+        
+    }
+    
 }
 
